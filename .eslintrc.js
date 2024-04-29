da4f74f7bdb7843b6cea387e550b83d6770ef861
mahamkalivenkata <166039347+mahamkalivenkata@users.noreply.github.com>
2024-04-29 13:53:03 -0700

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': 'off', // Disable no-unused-vars rule
+    'no-console': 'off', // Disable no-console rule
+    eqeqeq: ['error', 'always']
+  }
+};
